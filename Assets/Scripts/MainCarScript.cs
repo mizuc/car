4dbf442ccf012ed07043008fc8c1c79998288629
122D070 <shimizu.yyy24@gmail.com>
2024-12-18 04:22:31 +0900

    センサー可視化

--- Assets/Scripts/MainCarScript.cs
+++ Assets/Scripts/MainCarScript.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class MainCarScript : MonoBehaviour
 {
     Rigidbody2D rb;
-    private Material carMaterial;
     private Transform sensorTransform; // sensorの座標
+    private SensorScript sensorScript;
 
-    public float obstacleDetectDistance = 1f; // 100cm
-    public float carSpeed = 1f;
-    public float rotationSpeed = 50000f; // 回転するスピード
+    public float obstacleDetectDistance = 1; // 1m
+    public float carSpeed = 1;
+    public float rotationSpeed = 50000; // 回転するスピード
+    public float servoAngleDiff = 30;
 
     private float servoAngle = 0;
     private bool first_is = true;
@@ -23,10 +24,14 @@ public class MainCarScript : MonoBehaviour
         //Time.timeScale = 100f; // 100倍速で実行する
         Application.targetFrameRate = -1; // フレームレート制限を解除
         QualitySettings.vSyncCount = 0;   // V-Syncを無効化
+
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0f; //重力を0にする
         setCarMesh(); //車の見た目と当たり判定を定義
-        sensorTransform = GetComponentInChildren<Transform>(); //センサーの位置を取得
+
+        sensorScript = GetComponentInChildren<SensorScript>();
+        sensorScript.setSensorMesh(obstacleDetectDistance);
+        sensorTransform = GetComponentInChildren<Transform>();
     }
 
     void FixedUpdate()
@@ -43,22 +48,22 @@ public class MainCarScript : MonoBehaviour
         if (first_is)
         {
             servoAngle = 0f;
+            sensorScript.setSensorRotation(servoAngle);
+            yield return WaitForFixedFrames(45); // 450ms待つ センサーが回ってる時間
             first_is = false;
-            //yield return null; //1フレーム飛ばす
-            yield return WaitForFixedFrames(1); // 1フレーム飛ばす
         }
 
         float distance = GetDistance(servoAngle, obstacleDetectDistance);
-
         if (distance <= obstacleDetectDistance) // 近いなら
         {
-            yield return StartCoroutine(CarMotionControl("stop", 0)); //0frame待つ 意味ある?
+            //yield return StartCoroutine(CarMotionControl("stop", 0)); //0frame待つ 意味ある?
             for (int i = -2; i <= 2; i += 2)
             {
                 servoAngle = 30 * i; // -60, 0, 60
-                yield return WaitForFixedFrames(45); //45frames, 450ms待つ
+                sensorScript.setSensorRotation(servoAngle);
+                yield return WaitForFixedFrames(45); //450ms待つ センサーが回ってる時間
 
-                yield return WaitForFixedFrames(10); //10frames, 100ms待つ
+                yield return WaitForFixedFrames(10); //100ms待つ センサーが回ったのを止める時間
 
                 distance = GetDistance(servoAngle, obstacleDetectDistance);
 
@@ -67,8 +72,8 @@ public class MainCarScript : MonoBehaviour
                     yield return StartCoroutine(CarMotionControl("stop", 1));
                     if (i == 2)
                     {
-                        yield return StartCoroutine(CarMotionControl("backward", 50)); // 前進 0.5秒間実行
-                        yield return StartCoroutine(CarMotionControl("right", 5)); // 右に回転 0.05秒間実行
+                        yield return StartCoroutine(CarMotionControl("backward", 50));
+                        yield return StartCoroutine(CarMotionControl("right", 5));
                         first_is = true;
                     }
                 }
@@ -76,37 +81,33 @@ public class MainCarScript : MonoBehaviour
                 {
                     if (i == -2)
                     {
-                        yield return StartCoroutine(CarMotionControl("right", 5)); // 右に回転 0.05秒間実行
+                        yield return StartCoroutine(CarMotionControl("right", 5));
                     }
                     else if (i == 0)
                     {
-                        yield return StartCoroutine(CarMotionControl("forward", 5)); // 前進 0.05秒間実行
+                        yield return StartCoroutine(CarMotionControl("forward", 5));
                     }
                     else if (i == 2)
                     {
-                        yield return StartCoroutine(CarMotionControl("left", 5)); // 左に回転 0.05秒間実行
+                        yield return StartCoroutine(CarMotionControl("left", 5));
                     }
                     first_is = true;
                     break;
                 }
             }
         }
-        else // 遠いなら
+        else // 遠いなら 1フレーム前に進む
         {
-            yield return StartCoroutine(CarMotionControl("forward", 1)); // 前進 0.01秒間実行
+            yield return StartCoroutine(CarMotionControl("forward", 1));
         }
         isRunning = false;
     }
 
-    float GetDistance(float length, float dir)
+    float GetDistance(float distance, float dir)
     {
         int layerMask = ~LayerMask.GetMask("Ignore Raycast");
-
-        // dirを基準に方向を計算（0度を正面として角度を回転）
         Vector2 rayDirection = Quaternion.Euler(0, 0, dir) * sensorTransform.up;
         RaycastHit2D hit = Physics2D.Raycast(sensorTransform.position, rayDirection, Mathf.Infinity, layerMask);
-        // Rayを視覚化
-        Debug.DrawRay(sensorTransform.position, rayDirection * length, Color.red);
 
         if (hit.collider != null)
         {
@@ -178,7 +179,7 @@ public class MainCarScript : MonoBehaviour
 
         GetComponent<MeshFilter>().mesh = mesh;
 
-        carMaterial = new Material(Shader.Find("Unlit/Color"));
+        Material carMaterial = new Material(Shader.Find("Unlit/Color"));
         carMaterial.color = Color.white;
 
         GetComponent<MeshRenderer>().material = carMaterial;
--- Assets/Scripts/SensorScript.cs
+++ Assets/Scripts/SensorScript.cs
@@ -1,7 +1,38 @@
 using UnityEngine;
 
 public class SensorScript : MonoBehaviour
-{
-    private Material sensorMaterial;
-    private MainCarScript mainCarScript;
+{    public void setSensorMesh(float distance)
+    {
+        Mesh mesh = new Mesh();
+
+        // í∏ì_Çê›íË
+        Vector3[] vertices = new Vector3[4]
+        {
+            new Vector3(-0.01f, 0, 0), //ç∂â∫
+            new Vector3(0.01f, 0, 0), //âEâ∫
+            new Vector3(-0.01f, distance, 0), //ç∂è„
+            new Vector3(0.01f, distance, 0) //âEè„
+        };
+        mesh.vertices = vertices;
+
+        // ñ Çê›íË
+        int[] triangles = new int[6]
+        {
+            0, 2, 1,
+            2, 3, 1
+        };
+        mesh.triangles = triangles;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        Material sensorMaterial = new Material(Shader.Find("Unlit/Color"));
+        sensorMaterial.color = Color.yellow;
+
+        GetComponent<MeshRenderer>().material = sensorMaterial;
+    }
+
+    public void setSensorRotation(float angle)
+    {
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
 }
